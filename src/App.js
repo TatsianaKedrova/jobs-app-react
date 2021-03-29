2ad1c33cf735f3935da84fcf6c2318b8b408659a
Tatiana Kedrova <baletrot@gmail.com>
2021-03-29 11:05:55 +0200

    I commit all the changes at once

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import './App.css';
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
+import Job from "./Job";
+import JobsPagination from "./JobPagination";
+import SearchForm from "./SearchForm";
+
+
+function App() {
+
+    const [params, setParams] = useState({})
+    const [page, setPage] = useState(3)
+    const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+    function handleParamChange(e) {
+        const param = e.target.name
+        const value = e.target.value;
+        setPage(1)
+        setParams(prevParams => {
+            return {...prevParams, [param]: value }
+        })
+    }
+
+  return (
+    <Container className={"my-4"}>
+        <h1 className={"mb-4"}>GitHub Jobs</h1>
+        <SearchForm
+            params={params}
+            onParamChange={handleParamChange}
+        />
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>Error. Try refreshing!</h1>}
+        {jobs.map(job => {
+            return <Job key={job.id} job={job} />
+        })}
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+    </Container>
+  );
+}
+
+export default App;
